00f9601d25285920c822ae227bb070bed2f1756b
Germangalia <germangalia@iesebre.com>
2016-04-04 16:26:24 +0200

    Modify the database to insert API token

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -14,9 +14,9 @@
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
-        'title' => $faker->name,
         'email' => $faker->email,
         'password' => bcrypt(str_random(10)),
+        'api_token' => sha1(uniqid(rand(), true)),
         'remember_token' => str_random(10),
     ];
 });
